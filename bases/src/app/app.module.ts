bc2a465d7b4b648f94b7966b819db15e213813d3
soysergiogonza <soysergiogonza@gmail.com>
2025-02-02 15:47:08 -0500

    add: Se termina sección de bases de Angular

---
+++ bases/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { CounterModule } from './counter/components/counter.module';
+import { HeroesModule } from './heroes/components/heroes.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+    imports: [
+      BrowserModule,
+      AppRoutingModule,
+      CounterModule,
+      HeroesModule,
+    ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
